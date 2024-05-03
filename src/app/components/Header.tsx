567627d5b4247dd9adb2b606f7e4c7c7021da949
三溝英司 <mitsumizoeiji@bcD200855.local>
2024-05-03 21:22:00 +0900

    向き先変更

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -11,7 +11,7 @@ const Header = (props: HeaderProps) => {
     <header className="fixed top-0 w-full bg-lime-300 text-black text-3xl p-4 text-center shadow-md z-50">
       <Link href="/">{props.name}</Link>
       <Link
-        href="https://github.com/b-eiji-mitsumizo/stateManageLibCompare"
+        href="https://github.com/mitsumizo/react-state-manage-lib-compare"
         className="absolute right-4 top-2 font-bold py-2 px-4 rounded hover:text-gray-700"
         target="_blank"
         rel="noopener noreferrer"
